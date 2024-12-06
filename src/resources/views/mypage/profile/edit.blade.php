b78ace70556d839f5c50c3561515bd471267cd39
risamatsumoto1104 <risashibata114@gmail.com>
2024-12-06 22:14:02 +0900

    241206_22:13

--- src/resources/views/mypage/profile/edit.blade.php
+++ src/resources/views/mypage/profile/edit.blade.php
@@ -26,29 +26,38 @@
 @section('content')
     <div class="purchase-address-container">
         <h2 class="purchase-address-title">プロフィール設定</h2>
+        <div class="profile-image-container">
+            <div class="profile-image-wrapper">
+                <img class="profile-image" src="" alt="ユーザー画像"
+                    onerror="this.style.display='none'; this.parentElement.classList.add('profile-image-placeholder');">
+            </div>
+            <form class="profile-image-form" action="">
+                <label class="profile-image-button" for="profile-image-input">
+                    画像を選択する
+                </label>
+                <input class="profile-image-input" id="profile-image-input" type="file">
+            </form>
+        </div>
         <form class="purchase-address-form" action="">
             <div class="form-group">
                 <p class="form-label">ユーザー名</p>
                 <input class="form-input" type="text" placeholder="田中 太郎">
-                <p class="error-message">エラーメッセージの表示</p>
             </div>
             <div class="form-group">
                 <p class="form-label">郵便番号</p>
-                <input class="form-input" type="text" placeholder="田中 太郎">
-                <p class="error-message">エラーメッセージの表示</p>
+                <input class="form-input" type="text">
             </div>
             <div class="form-group">
                 <p class="form-label">住所</p>
                 <input class="form-input" type="text">
-                <p class="error-message">エラーメッセージの表示</p>
             </div>
             <div class="form-group">
                 <p class="form-label">建物名</p>
                 <input class="form-input" type="text">
-                <p class="error-message">エラーメッセージの表示</p>
             </div>
             <div class="form-submit">
                 <input class="submit-button" type="submit" value="更新する">
             </div>
         </form>
-    @endsection
+    </div>
+@endsection
